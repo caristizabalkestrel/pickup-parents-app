1faff02415ea88f80b0b384b16e8ad1d9663a456
Camilo Aristizabal Bedoya <camilo_aristizabal@outlook.com>
2025-05-21 22:28:58 -0500

    estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./auth/register/register.page').then(m => m.RegisterPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: 'students',
+    loadComponent: () => import('./students/students.page').then(m => m.StudentsPage)
   },
 ];
