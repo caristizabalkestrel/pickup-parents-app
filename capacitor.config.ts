c24ea52be03e1e8a5f35dbb813f5b52201d28803
Camilo Aristizabal Bedoya <camilo_aristizabal@outlook.com>
2025-05-21 20:34:03 -0500

    cargue inicial del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pickup-teachers-app',
+  webDir: 'www'
+};
+
+export default config;
